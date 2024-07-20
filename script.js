7347655a12ec95b38a62321ae73fa661a825b661
Ahmed Hussien98 <sandscorpion98@gmail.com>
2024-07-20 16:34:05 +0300

    Added To do List js project

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    
+    const addButton = document.getElementById("add-task-btn");
+
+    const taskInput = document.getElementById("task-input");
+
+    const taskList  = document.getElementById("task-list");
+    
+    
+    function addTask() {
+        
+        let taskText = taskInput.value.trim();
+
+        if (taskText === ""){
+            
+            window.alert("PLS Enter A Task.")
+        }else {
+
+            const listItem = document.createElement("li");
+            
+            listItem.textContent =taskText;
+
+            const newButton = document.createElement("button");
+
+            newButton.textContent = "Remove";
+
+            newButton.className = "remove-btn";
+
+            newButton.addEventListener("click", ()=> {
+                
+                taskList.removeChild(listItem);
+            })
+
+            listItem.appendChild(newButton);
+
+            taskList.appendChild(listItem);
+
+            taskInput.value = " ";
+        }
+    }
+
+    addButton.addEventListener("click", addTask);
+
+    taskInput.addEventListener("keypress", (event)=> {
+        if(event.key === "Enter"){
+            addTask();
+        }else {
+            return
+        }
+    });
+})
